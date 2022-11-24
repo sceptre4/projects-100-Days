7757f0056cbec3c2e67edfa1876b46b17640d688
sceptre <januarytrader@gmail.com>
2022-11-24 12:59:38 -0700

    ok

--- Day1.py
+++
@@ -1,8 +0,0 @@
-print("""Mark
-November 17, 2022
-I am signing on for Replit's 100 days of Python Challenge!
-I will make sure to spend some time every day coding along, for a minumum of 10 minutes a day.
-I'll be using Replit, an amazing onlin IDE so I can do this from phone whenever I happen to be.  No excuses for not coding from the middle of a field
-I am feeling :)
-You can follow my progress at replit.com/@StickEmUp
-""")
