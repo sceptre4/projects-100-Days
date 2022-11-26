babab95c07267ac6c2f6e3ef2cee4aaa6178b2bf
sceptre <januarytrader@gmail.com>
2022-11-26 14:29:25 -0700

    new lesson

---
+++ Day10.py
@@ -0,0 +1,8 @@
+# what generation are you from?
+born = int(input("When were you born?"))
+if born >= 1925 and born < 1947:
+    print("You belong to the Traditionalists")
+elif born >= 1947 and born < 1965:
+    print("You belong to the Baby Boomers")
+else:
+    print("whenever")
