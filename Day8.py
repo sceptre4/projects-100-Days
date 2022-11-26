f285180ab3db3e747932186e7c4052a892cd2a20
sceptre <januarytrader@gmail.com>
2022-11-26 13:28:15 -0700

    deleted new lesson

--- Day8.py
+++
@@ -1,7 +0,0 @@
-name = input("What's your name?")
-age = input("And " + (name) + "," + " how old are you?")
-if (age.isnumeric()) == True:
-    print("numeric")
-else:
-    print("Using Numbers Please.")
-    
