7dddbd03f7501f92eb10697b064078dc625a6d76
sceptre <januarytrader@gmail.com>
2022-11-27 22:02:57 -0700

    wrong day

--- Day10.py
+++ Day10.py
@@ -1,15 +1,5 @@
-# what generation are you from?
-born = int(input("When were you born?"))
-if born >= 1925 and born < 1947:
-    if born==1940:
-        print("born in 1940")
-    elif born>=1941:
-        print("born between 1941 and 1946")
-    elif born>=1925 and born<=1939:
-        print("born 1925 to 1939")
-    else:
-        print("born between 25 and 40 sometime")
-elif born >= 1947 and born < 1965:
-    print("You belong to the Baby Boomers")
-else:
-    print("whenever")
+#print (343%4)
+myBill = float(input("What was the bill?: "))
+numberOfPeople = int(input("How many people?: "))
+answer = myBill / numberOfPeople
+print (round("You all owe", answer,))
