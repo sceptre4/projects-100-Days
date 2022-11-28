f203adeb664694409a1e0e58e8de6e45286300a2
sceptre <januarytrader@gmail.com>
2022-11-28 16:20:50 -0700

    workingDay11LeapYear

--- Day11-SecondsInAYear.py
+++ Day11-SecondsInAYear.py
@@ -4,24 +4,20 @@ year = int(input("What year is it?"))
 secs = 60
 min = 60
 hour = 24
-daysecs = (secs)*(min)*(hour)
+daySecs = (secs)*(min)*(hour)
 
-regyearsecs = 365*(daysecs)
-leapyearsecs = 366*(daysecs)
-
-
-leapyear = ""
+regYearSecs = 365*(daySecs)
+leapYearSecs = 366*(daySecs)
 
 
-if year % 400 == 0 and year % 100 == 0 and year % 4 == 0:
-    print("There are", leapyearssecs, "seconds in this leap year.400")
-elif year % 4 == 0:
-    print("There are", leapyearssecs, "seconds in this leap year.4")
-elif year % 4 == 0:
-        print("Yes, it's 400 100 and 4")
+if year%4==0:
+    if (year%100==0 and year%400!=0):
+        print(str(year)+" isn't a Leap Year, so it has 365 days in it or",regYearSecs,"seconds. 100yes 400no")
+    elif year%100==0 and year%400==0:
+        print(str(year)+" is a Leap Year, so it has 366 days in it or",leapYearSecs,"seconds.")
+    else:
+        print(str(year)+" is a Leap Year, so it has 366 days in it or",leapYearSecs,"seconds by 4.")
+
+
 else:
-        print("no, divisable by 100 and 400, but not 4")
-elif year % 4 == 0:
-        print("Yes, its leap")
-
-"""
+    print(str(year)+" isn't a Leap Year, so it has 365 days in it or",regYearSecs,"seconds. It isn't divisable by 4 evenly.")
