da6a764d963d5ce0c9a93e4ac2efa4ef1692ff96
sceptre <januarytrader@gmail.com>
2022-11-28 19:25:27 -0700

    add%

--- Day10.py
+++ Day10.py
@@ -1,5 +1,9 @@
-#print (343%4)
 myBill = float(input("What was the bill?: "))
 numberOfPeople = int(input("How many people?: "))
+tipAmount = float(input("What % tip should they get?"))
+
+tipAmount = tipAmount/100
+myBill = myBill*(1+tipAmount)
 answer = myBill / numberOfPeople
-print (round("You all owe", answer,))
+answer = round(answer, 2)
+print("You each owe $"+str(answer)+".")
