ce412ade63bb507a873d661fec417a3892d7ce59
sceptre <januarytrader@gmail.com>
2022-12-03 17:21:04 -0700

    random number doesn't clear

--- Day18-Guess-Number.py
+++ Day18-Guess-Number.py
@@ -5,33 +5,39 @@ from random import randint
 seed(1)
 # generate some integers
 for _ in range(1):
-    randNumber = randint(1, 1000000)
-    # print(randNum)
+    randNumber = randint(1, 5)
+   
 attempt = 0
 print(randNumber)
 
-guess = int(input("Between 1 and a Million"))
-print(guess)
+
+
 
 while True:
-    guess = int(input("Between 1 and a Million"))
+    guess = int(input("Between 1 and a Million  "))
     print(guess)
-
+    print()
     if guess < 0:
+        print("you're done")
         quit()
     elif guess == randNumber:
         print("Winner")
         attempt += 1
+        print()
         break
     elif guess < randNumber:
         print("YOur guess is Too Small")
         attempt += 1
+        print()
         continue
     elif guess > randNumber:
         print("Your guess is Too Big")
         attempt += 1
+        print()
         continue
     else:
         print("why")
+        print()
         break
 print("the number was guessed in", attempt, "tries.")
+randNumber=""
