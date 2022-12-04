c891d31fc7d9e3b0757866c7b5ceb7b4507d32b7
sceptre <januarytrader@gmail.com>
2022-12-03 17:37:19 -0700

    not random

--- Day18-Guess-Number.py
+++ Day18-Guess-Number.py
@@ -1,12 +1,6 @@
-# generate random integer values
-from random import seed
-from random import randint
-# seed random number generator
-seed(1)
-# generate some integers
-for _ in range(1):
-    randNumber = randint(1, 5)
-   
+
+
+
 attempt = 0
 print(randNumber)
 
