f2b779717bcbb9a547fb38508eb0f15991ce20ff
sceptre <januarytrader@gmail.com>
2022-12-05 12:50:27 -0700

    done

---
+++ Day22-Libraries.py
@@ -0,0 +1,4 @@
+import random
+#unlike the range function, this one includes the second parameter in its eligble output. range would have been 1 less than 10. so loops need to remoember that.
+myNumber=random.randint(1,10)
+#my solution to this assignment was done by me in Day 18.
