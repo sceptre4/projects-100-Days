bf43789f04fa06edd4e56048cc14020f87eefa76
sceptre <januarytrader@gmail.com>
2022-12-06 23:16:15 -0700

    trying

---
+++ Day25-Return.py
@@ -0,0 +1,22 @@
+'''
+def pinpicker(number):
+    import random
+    pin=""
+    for i in range(number):
+        pin=pin+str(random.randint(0,9))
+    return pin
+
+myPin=pinpicker(9)
+print(myPin)
+'''
+import random
+roll=""
+highSide=int(input("Pick how many sides"))
+
+def firstDice("1",outcome):
+
+   print(firstDice)
+
+outcome=firstDice("1",random.randint(1,highSide))
+
+print(outcome)
